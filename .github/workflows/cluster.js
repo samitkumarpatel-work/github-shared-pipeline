f6c8df3f6d6219a91bcd2e612ca6c1c13b1d6a39
samit-maersk <SamitKumar.Patel@maersk.com>
2022-11-04 22:26:24 +0100

    pipeline test

---
+++ .github/workflows/cluster.js
@@ -0,0 +1,15 @@
+module.exports = ({github, context}) => {
+    return [
+        {
+            "name": "dev-1",
+            "namespace": "price",
+            "clusterName": "xp1"
+        },
+        {
+            "name": "dev-2",
+            "namespace": "price",
+            "clusterName": "xp2"
+        }
+    ]
+}
+
